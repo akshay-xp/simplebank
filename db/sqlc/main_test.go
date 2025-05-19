008846c99a9fa6b48f2599d12581c250c1cee5ff
akshay.paka <psakshay2022@gmail.com>
2025-05-19 22:08:27 +0530

    init project

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbSource = "postgresql://root:enterpi@localhost:5433/simple_bank?sslmode=disable"
+)
+
+var testStore Store
+
+func TestMain(m *testing.M) {
+	connPool, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to db", err)
+	}
+
+	testStore = NewStore(connPool)
+	os.Exit(m.Run())
+}
