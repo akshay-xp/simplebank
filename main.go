879c37d88f837115efbc91e1f1fba319843f0846
akshay.paka <psakshay2022@gmail.com>
2025-05-29 22:45:11 +0530

    implement login api

--- main.go
+++ main.go
@@ -4,27 +4,30 @@ import (
 	"context"
 	"log"
 
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/akshay-xp/simplebank/api"
 	db "github.com/akshay-xp/simplebank/db/sqlc"
 	"github.com/akshay-xp/simplebank/util"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("Connot load config:", err)
+		log.Fatal("connot load config:", err)
 	}
 	connPool, err := pgxpool.New(context.Background(), config.DBSource)
 	if err != nil {
-		log.Fatal("Cannot connect to db", err)
+		log.Fatal("cannot connect to db", err)
 	}
 
 	store := db.NewStore(connPool)
-	server := api.NewServer(store)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
 
 	err = server.Start(config.ServerAddress)
 	if err != nil {
-		log.Fatal("Cannot start server:", err)
+		log.Fatal("cannot start server:", err)
 	}
 }
