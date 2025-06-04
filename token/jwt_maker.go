fe3d315834f71ef8ad6f300a1ec6fc851f921280
akshay.paka <psakshay2022@gmail.com>
2025-06-04 19:05:58 +0530

    renew session with refresh token

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -24,15 +24,15 @@ func NewJWTMaker(secretkey string) (Maker, error) {
 }
 
 // CreateToken creates a token for a specific username and duration
-func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
 	payload, err := NewPayload(username, duration)
 	if err != nil {
-		return "", err
+		return "", payload, err
 	}
 
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 	token, err := jwtToken.SignedString([]byte(maker.secretkey))
-	return token, err
+	return token, payload, err
 }
 
 // VerifyToken checks if the token is volid or not
